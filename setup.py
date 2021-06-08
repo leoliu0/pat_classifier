666770d48856ba4d3eedea9c7de12181bf77a222
Leo Liu <auleo.liu@gmail.com>
2021-06-08 15:41:35 +1000

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pat_classifier',
+    version='1.0',
+    description='patent type classifier',
+    author='Leo Liu',
+    #  author_email='gward@python.net',
+    #  url='https://www.python.org/sigs/distutils-sig/',
+    packages=['pat_classifier'],
+    package_data={'pat_classifier': ['*.txt']},
+)
