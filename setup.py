4fa9aea60e808dee154a846f3bdd4a7bb86c64a4
Leo Liu <auleo.liu@gmail.com>
2022-05-02 13:58:14 +1000

    add dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='pat_classifier',
-    version='1.0',
-    description='patent type classifier',
-    author='Leo Liu',
+    name="pat_classifier",
+    version="1.0",
+    description="patent type classifier",
+    author="Leo Liu",
     #  author_email='gward@python.net',
     #  url='https://www.python.org/sigs/distutils-sig/',
-    packages=['pat_classifier'],
-    package_data={'pat_classifier': ['*.txt']},
+    packages=["pat_classifier"],
+    package_data={"pat_classifier": ["*.txt"]},
+    install_requires=["wordcloud", "loguru", "pyspark", "pandas"],
 )
