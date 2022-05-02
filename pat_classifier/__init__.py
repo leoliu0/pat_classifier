3e757b802f6bd717636bfd5df11c8911f09aa2be
Leo Liu <auleo.liu@gmail.com>
2022-05-02 14:36:37 +1000

    add dependencies

--- pat_classifier/__init__.py
+++ pat_classifier/__init__.py
@@ -1,5 +1,6 @@
 from pyspark import *
 from pyspark.sql import *
+import pyspark.sql.functions as F
 from pyspark.sql.types import *
 from pyspark.sql.window import Window
 import pyspark.sql.functions as F
@@ -9,55 +10,82 @@ import pandas as pd
 import re
 from ._word_list import w_proc
 
-stopwords = set(STOPWORDS) | set(['there\w+', 'the'])
-stopwords_regex = r'\b' + r'\b|\b'.join(stopwords) + r'\b'
+stopwords = set(STOPWORDS) | set(["there\w+", "the"])
+stopwords_regex = r"\b" + r"\b|\b".join(stopwords) + r"\b"
 
-additional_proc_words = resources.open_text('pat_classifier',
-                                            'additional_proc_words.txt')
-w_proc2 = r'|'.join(
+additional_proc_words = resources.open_text("pat_classifier", "more_proc_words.txt")
+w_proc2 = r"|".join(
     list(
-        pd.read_csv(additional_proc_words, sep=' ',
-                    names=['w', 'n']).w.str.lower().map(lambda s: str(s))))
+        pd.read_csv(additional_proc_words, sep=" ", names=["w", "n"])
+        .w.str.lower()
+        .map(lambda s: str(s))
+    )
+)
 
-w_proc_re = re.compile(w_proc + '|' + w_proc2)
+w_proc_re = w_proc + "|" + w_proc2
+# w_proc_re = re.compile(w_proc + '|' + w_proc2)
 
 wl_proc = [
-    x.replace('\\b', '').replace('$', '').replace('s?', '').replace('^', '')
-    for x in (w_proc + '|' + w_proc2).split('|')
+    x.replace("\\b", "").replace("$", "").replace("s?", "").replace("^", "")
+    for x in (w_proc + "|" + w_proc2).split("|")
 ]
 
-product_words_list = resources.open_text('pat_classifier',
-                                         'product_words_list.txt')
-w_prod_re = re.compile(r'\b' + r'|\b'.join(
+product_words_list = resources.open_text("pat_classifier", "product_words_list.txt")
+w_prod_re = r"|".join(
     list(
-        pd.read_csv(product_words_list, sep=' ', names=['w', 'n']).w.str.lower(
-        ).map(lambda s: str(s) + r's?$'))))
+        pd.read_csv(product_words_list, sep=" ", names=["w", "n"])
+        .w.str.lower().str.replace('_',' ')
+        .map(lambda s: r"\b" + str(s) + r"s?$")
+    )
+)
 
-
-def pre_process(pat):
-    pat = pat.withColumn("title", F.lower("title"))  # lower case
+def pre_process(pat, strcolumn):
+    pat = pat.withColumn(strcolumn, F.lower(strcolumn))  # lower case
     pat = pat.withColumn(  # remove improvement related phrase
-        "title",
-        F.regexp_replace(F.col("title"),
-                         "(improvement|enhancement) (in|on|of|for)", ""))
+        strcolumn,
+        F.regexp_replace(
+            F.col(strcolumn), "(improvement|enhancement) (in|on|of|for)", ""
+        ),
+    )
     pat = pat.withColumn(  # remove and between verbs so "and" can split correctly
-        "title",
-        F.regexp_replace(F.col("title"), r"(\w+ing) and (\w+ing)", r"\1 \2"))
+        strcolumn,
+        F.regexp_replace(F.col(strcolumn), r"(\w+ing) and (\w+ing)", r"\1 \2"),
+    )
     pat = pat.withColumn(  # split by comma and "and" to get partitions
-        'title', F.split('title', ',| and '))
+        strcolumn, F.split(strcolumn, "(?!.*ing),| and ")
+    )
     pat = pat.withColumn(  # remove part after prepositions
-        'title', F.explode('title'))
+        strcolumn, F.explode(strcolumn)
+    )
 
-    split_by = ' of | for | having | with | using | used | to | on | in | based | via | by | including | comprising | comprised | provide | provided | providing | produce | producing | which | that '
+    split_by = " of | for | having | with | using | used | to | on | in | based | via | by | including | comprising | comprised | provide | provided | providing | produce | producing | which | that "
 
     pat = pat.withColumn(  # split by conjection words
-        'title',
-        F.split('title', split_by).getItem(0))
+        strcolumn, F.split(strcolumn, split_by).getItem(0)
+    )
     pat = pat.withColumn(  # remove stopwords
-        'title', F.regexp_replace(F.col('title'), stopwords_regex, ''))
-    pat = pat.withColumn(  # additional cleanup
-        'title', F.trim('title'))
+        strcolumn, F.regexp_replace(F.col(strcolumn), stopwords_regex, "")
+    )
+    pat = pat.withColumn(strcolumn, F.trim(strcolumn))  # additional cleanup
     pat = pat.withColumn(
-        'title', F.regexp_replace(F.col('title'), r'\.|,|produced$', ''))
-    pat = pat.dropna().toPandas()
+        strcolumn, F.regexp_replace(F.col(strcolumn), r"\.|,|produced$", "")
+    )
+    pat = pat.dropna()
     return pat
+
+
+def classify(df, strcolumn, debug=False):
+    df = pre_process(df, strcolumn)
+    df = df.withColumn("process_word", F.regexp_extract(strcolumn, w_proc_re, 0))
+    df = df.withColumn("product_word", F.regexp_extract(strcolumn, w_prod_re, 0))
+
+    df = df.withColumn("product", F.when(F.col("product_word") != "", 1).otherwise(0))
+    df = df.withColumn(
+        "process",
+        F.when(
+            (F.col("product_word") == "") & (F.col("process_word") != ""), 1
+        ).otherwise(0),
+    )
+    df = df.withColumn("product", F.when(F.col("process") == 0, 1).otherwise(0))
+
+    return df
