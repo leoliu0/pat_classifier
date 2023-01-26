b7a683058a561a44721935c6b4d23f585dc225f2
Leo Liu <auleo.liu@gmail.com>
2023-01-27 10:52:17 +1100

    add gitignore

--- pat_classifier/title.py
+++ pat_classifier/title.py
@@ -1,8 +1,8 @@
-from . import split_words
-from . import w_proc_re, w_prod_re, split_words
+from .common import split_words, w_proc_re, w_proc, w_proc_more, w_prod_re, split_words
 from collections import namedtuple
 import re
 from typing import List, Dict
+from nltk import pos_tag, word_tokenize
 
 re_imp = re.compile("(improvement|enhancement)\s+(in|on|of|for)")
 re_w_and = re.compile(r" \w+ing,?\s+and")
@@ -16,21 +16,47 @@ w_prod_re = re.compile(w_prod_re)
 Classification = namedtuple("Classification", "i, part, process")
 
 
-def classify_partition(a):
-    if a.endswith("production line"):
+def classify_partition(a: str, dbg) -> int:
+    if (
+        "method and system" in a
+        or "methods and system" in a
+        or "system and method" in a
+    ):
         return 1
-    if w_prod_re.search(a):
-        return 0
+
+    a = " ".join(
+        [x for x, p in pos_tag(word_tokenize(a)) if p != "RB" and wplus.search(x)]
+    ).strip()
+    if dbg:
+        print(a)
     try:
-        a = wplus.findall(a)[-1].strip()
+        first_word = wplus.findall(a)[0].strip()
+        last_word = wplus.findall(a)[-1].strip()
     except:
         return
-    if w_proc_re.search(a):
+
+    if w_prod_re.search(a):
+        return 0
+
+    if w_proc_re.search(first_word):
+        if dbg:
+            print(f"first word process", w_proc_re.search(first_word))
         return 1
+
+    if w_proc_re.search(last_word):
+        if dbg:
+            print(f"last word process", w_proc_re.search(last_word))
+        return 1
+
+    # for w in w_proc + w_proc_more:  # first tier of process words
+    #     if a.endswith(w):
+    #         if dbg:
+    #             print(f"last word is process {w}")
+    #         return 1
     return 0
 
 
-def classify_title(s: str) -> List:
+def classify_title(s: str, dbg=0) -> List:
     result = []
     s = s.lower()
     s = re_imp.sub("", s)
@@ -39,5 +65,5 @@ def classify_title(s: str) -> List:
     s = re_w_and.sub("", s)
     for i, part in enumerate(s.split(" and "), start=1):
         part = re_split.split(part)[0]
-        result.append(Classification._make([i, part, classify_partition(part)]))
+        result.append(Classification._make([i, part, classify_partition(part, dbg)]))
     return result
--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from distutils.core import setup
 
 setup(
     name="pat_classifier",
-    version="1.0",
+    version="1.1",
     description="patent type classifier",
     author="Leo Liu",
+    #  author_email='gward@python.net',
+    #  url='https://www.python.org/sigs/distutils-sig/',
     install_requires=["wordcloud", "pandas", "nltk"],
     packages=["pat_classifier"],
     package_data={"pat_classifier": ["*.txt"]},
